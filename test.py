1ad787f3c5c225876ca3df71fff0f4d9411e9bff
Audrey Andoy <audrey@Audreys-MacBook-Air.local>
2020-11-30 04:39:27 -0800

    no longer need superliner.yml or test.py, figured out the actions on my own

--- test.py
+++
@@ -1,6 +0,0 @@
-'''
-testing actions part 2, seems like i forgot to pull so that this branch has .github/label.yml
-okay now it seems like I'm not using the right labeler version via 'uses'
-THERES A LABELER V3 THAT JUST CAME OUT LIKE TODAY AHHH
-
-'''
